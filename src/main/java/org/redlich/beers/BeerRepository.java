2553b7b1901c9f9073c1952021472992fa931ffd
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-02 15:29:34 -0300

    update reposiories
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/redlich/beers/BeerRepository.java
+++ src/main/java/org/redlich/beers/BeerRepository.java
@@ -1,29 +1,18 @@
 package org.redlich.beers;
 
+import jakarta.nosql.mapping.Repository;
+
+import javax.enterprise.context.ApplicationScoped;
 import java.util.stream.Stream;
 
-/*/
-import jakarta.nosql.mapping.Repository;
-import jakarta.nosql.mapping.Page;
-import jakarta.nosql.mapping.Pagination;
-/*/
-import jakarta.data.repository.Repository;
-import jakarta.data.repository.Page;
-import jakarta.data.repository.Pageable;
-
-// import javax.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.context.ApplicationScoped;
-
 @ApplicationScoped
-public interface BeerRepository extends Repository { // <Beer, String> {
+public interface BeerRepository extends Repository<Beer, Integer> {
 
     Stream<Beer> findAll();
 
-    Page<Beer> findAll(Pageable pagination);
 
     Stream<Beer> findByName(String beer);
 
     Stream<Beer> findByBrewerId(int brewer_id);
 
-    void deleteById(int id);
 }
